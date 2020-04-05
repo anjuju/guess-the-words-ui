0d6f9d30a359bf85f2f0476fada347e11d48b970
john huynh <johhuynh@corelogic.com>
2020-04-04 19:53:13 -0700

    feat: socket io

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-
+import socketIOClient from 'socket.io-client';
 import Game from './Components/Game/Component';
+const socketEndPoint = 'http://localhost:8080/';
+
+const ioSocket = socketIOClient(socketEndPoint); 
+const ns= 'public_chat';
+const salaSocket = socketIOClient(socketEndPoint + ns)
+const messageKey = 'test_message'; // Message id
+
+
+function sendMessage(){
+  salaSocket.emit(messageKey, 'Prueba de mensaje desde el cliente: '+Math.random())
+}
+
+function recepMessage(){
+  salaSocket.on(messageKey, (received) => {
+    alert('Message: '+ received)
+  })
+}
 
 function App() {
+  useEffect(() => recepMessage() , [])
   return (
     <div className="App">
+      <div className="App">
+        <button style={{marginTop: '50', padding: '10px'}} onClick={sendMessage}>
+          Send message
+        </button>
+      </div>
       <header>
         <h1>Guess the Words</h1>
       </header>
